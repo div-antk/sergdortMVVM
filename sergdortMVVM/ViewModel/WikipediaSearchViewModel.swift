febc0c221523411689c015a90d7f2e456f96539f
div-antk <51283468+owaii@users.noreply.github.com>
2021-08-31 21:30:39 +0900

    ViewModelType作成

---
+++ sergdortMVVM/ViewModel/WikipediaSearchViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  WikipediaSearchViewModel.swift
+//  sergdortMVVM
+//
+//  Created by Takuya Ando on 2021/08/31.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class WikipediaSearchViewModel{
+    private let disposeBag = DisposeBag()
+    
+    private let scheduler: SchedulerType
+}
+
+extension WikipediaSearchViewModel: ViewModelType {
+    
+}
