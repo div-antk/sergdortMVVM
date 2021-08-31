77240e768582ffccf622a5acc323cbe1099d5a09
div-antk <51283468+owaii@users.noreply.github.com>
2021-08-31 20:36:57 +0900

    Initial Commit

---
+++ sergdortMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sergdortMVVM
+//
+//  Created by Takuya Ando on 2021/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
